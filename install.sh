30385f3f871725749feb3fc7788648f3b9e72837
Mike Senn <msenn@salesforce.com>
2024-02-24 10:12:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
 # Configure powerlevel10k zsh theme
-ln .zshrc .p10k.zsh $HOME/
+ln .zshrc $HOME/.zshrc
+ln .p10k.zsh $HOME/.p10k.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
